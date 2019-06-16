43e75b699c0ff4ab4235153b885fa8386385dba1
admin <jonahcasey@icloud.com>
2019-06-15 20:31:05 -0400

    Testing github.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 public static void main(String[] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
-        //Testing
+        //Testing2
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
                         public void run() {
                                 BoardFrame frame = new BoardFrame("Minesweeper");
