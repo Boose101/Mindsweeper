e85cc9bf76df2ca041cd178c5db765ccdb01a32b
Boose101 <51867298+Boose101@users.noreply.github.com>
2022-08-04 13:17:47 -0400

    Update SizeGUI.java

--- src/main/java/minesweeper/SizeGUI.java
+++ src/main/java/minesweeper/SizeGUI.java
@@ -11,52 +11,52 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class SizeGUI {
-   public static void main(String[] args) {
-      createWindow();
+	public static void main(String[] args) {
+    	createWindow();
    }
 
     public static void createWindow() {    
-      JFrame frame = new JFrame("Size");
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      createUI(frame);
-      frame.setSize(100, 200);      
-      frame.setLocationRelativeTo(null);  
-      frame.setVisible(true);
+		JFrame frame = new JFrame("Size");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		createUI(frame);
+		frame.setSize(100, 200);      
+    	frame.setLocationRelativeTo(null);  
+    	frame.setVisible(true);
    }
 
    private static int createUI(final JFrame frame){  
-      JPanel panel = new JPanel();
-      LayoutManager layout = new FlowLayout();  
-      panel.setLayout(layout);       
+		JPanel panel = new JPanel();
+		LayoutManager layout = new FlowLayout();  
+		panel.setLayout(layout);       
 
-      JButton button = new JButton("Start Game");
-      final JLabel label = new JLabel();
-      button.addActionListener(new ActionListener() {
-         @Override
-         public void actionPerformed(ActionEvent e) {
-            String result = (String) JOptionPane.showInputDialog(
-               frame,
-               "Enter the size:", 
-               "Size",            
-               JOptionPane.PLAIN_MESSAGE,
-               null,            
-               null, 
-               "20"
-            );
-            System.out.println(result);
-            if(result != null){
-               label.setText("You selected:" + result);
-            }else {
-               label.setText("None selected");
-               result = "20";
+		JButton button = new JButton("Start Game");
+		final JLabel label = new JLabel();
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String result = (String) JOptionPane.showInputDialog(
+					frame,
+				"Enter the size:", 
+				"Size",            
+				JOptionPane.PLAIN_MESSAGE,
+				null,            
+				null, 
+				"20"
+				);
+				System.out.println(result);
+				if(result != null){
+				label.setText("You selected:" + result);
+				}else {
+				label.setText("None selected");
+				result = "20";
 
-            }
-         }
-      });
+				}
+			}
+		});
 
-      panel.add(button);
-      panel.add(label);
-      frame.getContentPane().add(panel, BorderLayout.CENTER); 
-      return 20; 
-   }  
+		panel.add(button);
+		panel.add(label);
+		frame.getContentPane().add(panel, BorderLayout.CENTER); 
+		return 20; 
+	}  
 } 
